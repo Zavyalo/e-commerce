f37957416fac4cf24ab863e674a175bf0caf35b0
Студенческая <@rbiu.local>
2025-04-01 16:53:38 +0500

    bag

--- js/bag.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const menu = document.querySelector(".header")
-const burgerBtn = document.querySelector(".burger")
-
-burgerBtn.addEventListener('click', ()=>{
-    menu.classList.toggle("bag--open"); //добавить класс
-})
